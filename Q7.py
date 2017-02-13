18aeb5dfc0ddc2bae69dce3564bdef129df5084e
gandhijay <150303105046@paruluniversity.ac.in>
2017-02-13 21:59:57 +0530

    Create Q7.py

---
+++ Q7.py
@@ -0,0 +1,6 @@
+    angle1=float(input( " Enter the first angle of the triangle "))
+    angle2=float(input( " Enter the second angle of the triangle  "))
+    def thirdangle(angle1,angle2) :
+         angle3=180-(angle1+angle2)
+         print ( " Third angle of triangle is ", angle3)
+    thirdangle(angle1 , angle2 )
